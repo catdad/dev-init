5129a2d588434847f765defe31b9a5b2f8c10407
Kiril Vatev <vatev.1@gmail.com>
2016-07-10 19:48:58 -0400

    organizing, getting ready for conditionally running modules

--- index.js
+++ index.js
@@ -6,20 +6,30 @@ var async = require('async');
 
 global.__base = path.resolve(__dirname);
 
+var list = {
+    git: require('./src/init.js'),
+    npm: require('./src/npm-init.js'),
+    brackets: require('./src/brackets.js'),
+    editorconfig: require('./src/editorconfig.js'),
+    gitignore: require('./src/gitignore.js'),
+    gitattributes: require('./src/gitattributes.js'),
+    readme: require('./src/readme.js'),
+};
+
 module.exports = function index(opts, done) {
-    var list = [
-        require('./src/init.js'),
-//        require('./src/npm-init.js'),
-        require('./src/brackets.js'),
-        require('./src/editorconfig.js'),
-        require('./src/gitignore.js'),
-        require('./src/gitattributes.js'),
-        require('./src/readme.js'),
+    // keep the list in order
+    var tasks = [
+        list.git,
+        list.brackets,
+        list.editorconfig,
+        list.gitignore,
+        list.gitattributes,
+        list.readme
     ].map(function(mod) {
         return function callModule(next) {
             return mod(opts, next);
         };
     });
 
-    async.series(list, done);
+    async.series(tasks, done);
 };
