e84bbb2a38de0cc2de068c6c2210766f4a24ccf8
Kiril Vatev <vatev.1@gmail.com>
2016-07-09 20:22:57 -0400

    adding index file to run all the modules in order

---
+++ index.js
@@ -0,0 +1,27 @@
+/* jshint node: true */
+
+var path = require('path');
+
+var async = require('async');
+var argv = require('yargs').argv;
+
+global.__base = path.resolve(__dirname);
+
+var list = [
+    require('./src/init.js'),
+    require('./src/brackets.js')
+].map(function(mod) {
+    return function callModule(next) {
+        return mod(argv, next);
+    };
+});
+
+async.series(list, function(err) {
+    if (err) {
+        console.error(err);
+        process.exitCode = 1;
+        return;
+    }
+    
+    console.log('Done!');
+});
