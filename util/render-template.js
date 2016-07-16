87ffbf5950caec699478caa4974a64f3589cc7e6
Kiril Vatev <vatev.1@gmail.com>
2016-07-16 10:27:06 -0400

    added some comments

--- util/render-template.js
+++ util/render-template.js
@@ -3,11 +3,14 @@
 var _ = require('lodash');
 
 function render(str, data) {
+    // to be responsible, save the default tempalte
+    // settings in lodash
     var settings = _.clone(_.templateSettings);
     _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
     var rendered = _.template(str)(data);
 
+    // restore the default settings
     _.templateSettings = settings;
 
     return rendered;
