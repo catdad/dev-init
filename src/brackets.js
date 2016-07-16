1bf082a249304a7f3ad08c4b6070ee263546022e
Kiril Vatev <vatev.1@gmail.com>
2016-07-16 10:22:15 -0400

    brackets.json file will now be merged

--- src/brackets.js
+++ src/brackets.js
@@ -2,18 +2,45 @@
 /* global __base */
 
 var path = require('path');
+var _ = require('lodash');
 
-var renderFile = require(path.posix.join(__base, 'util', 'renderFile.js'));
+var mergeFile = require(path.posix.join(__base, 'util', 'mergeFile.js'));
 
 var DEFAULT_LINTER = ['JSHint'];
 
+function customizer(objValue, srcValue) {
+    if (_.isArray(objValue)) {
+        return _.uniq(objValue.concat(srcValue));
+    }
+}
+
+function mergeJson(existing, source) {
+    var eData;
+    var sData;
+
+    try {
+        eData = JSON.parse(existing);
+        sData = JSON.parse(source);
+    } catch(err) {
+        console.log('merge error', err, source);
+        // avoid overwriting the existing file
+        // if we cannot parse it
+        return existing;
+    }
+
+    var merged = _.mergeWith(sData, eData, customizer);
+
+    return JSON.stringify(merged, undefined, 4);
+}
+
 module.exports = function bracketsFile(opts, done) {
-    renderFile({
+    mergeFile({
         source: path.resolve(__base, 'fixtures/brackets.json'),
         dest: path.resolve('.', '.brackets.json'),
         data: {
             linter: JSON.stringify(opts.linter || DEFAULT_LINTER)
         },
-        argv: opts
+        argv: opts,
+        mergeFunction: mergeJson
     }, done);
 };
