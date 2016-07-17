215e3beaa0d443051ec2a4915fd5d91ed3ec8812
Kiril Vatev <vatev.1@gmail.com>
2016-07-17 08:23:43 -0400

    merging editorconfig files by default

--- src/editorconfig.js
+++ src/editorconfig.js
@@ -2,13 +2,32 @@
 /* global __base */
 
 var path = require('path');
+var ec = require('editorconfig-parser');
+var _ = require('lodash');
 
-var renderFile = require(path.posix.join(__base, 'util', 'render-file.js'));
+var mergeFile = require(path.posix.join(__base, 'util', 'merge-file.js'));
+
+function merge(existing, source) {
+    var eData;
+    var sData;
+
+    try {
+        eData = ec.parse(existing);
+        sData = ec.parse(source);
+    } catch(err) {
+        return existing;
+    }
+
+    var merged = _.merge(eData, sData);
+
+    return ec.serialize(merged);
+}
 
 module.exports = function bracketsFile(opts, done) {
-    renderFile({
+    mergeFile({
         source: path.resolve(__base, 'fixtures/editorconfig'),
         dest: path.resolve('.', '.editorconfig'),
-        argv: opts
+        argv: opts,
+        mergeFunction: merge
     }, done);
 };
