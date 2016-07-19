952c513626e6a594efacd1dddd1a97202c4bf811
Kiril Vatev <vatev.1@gmail.com>
2016-07-19 07:23:31 -0400

    first implementation of gitignore merge

--- src/gitignore.js
+++ src/gitignore.js
@@ -2,13 +2,39 @@
 /* global __base */
 
 var path = require('path');
+var lines = require('line-merge');
 
-var renderFile = require(path.posix.join(__base, 'util', 'render-file.js'));
+var mergeFile = require(path.posix.join(__base, 'util', 'merge-file.js'));
+
+function validateStr(str) {
+    if (!str || typeof str !== 'string' || str.trim() === '') {
+        return '';
+    }
+
+    return str;
+}
+
+// .gitignore is just a list of patterns, so we can actually merge
+// all the lines and everything will be fine. I'll still try to
+// preserve comments though.
+function merge(existing, source) {
+    existing = validateStr(existing);
+    source = validateStr(source);
+
+    var eData = lines.tokenize(existing);
+
+    console.log(eData);
+
+    throw new Error('do nothing');
+
+    return lines.merge(existing, source);
+}
 
 module.exports = function bracketsFile(opts, done) {
-    renderFile({
+    mergeFile({
         source: path.resolve(__base, 'fixtures/gitignore'),
         dest: path.resolve('.', '.gitignore'),
-        argv: opts
+        argv: opts,
+        mergeFunction: merge
     }, done);
 };
