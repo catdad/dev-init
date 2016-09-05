1604575e7632225fa283cc7d0a8b5d78a7388367
Kiril Vatev <vatev.1@gmail.com>
2016-09-05 14:45:14 -0400

    rewriting the crap out of npm-test script
    #9

--- src/npm-test.js
+++ src/npm-test.js
@@ -7,7 +7,7 @@ var path = require('path');
 var shellton = require('shellton');
 var async = require('async');
 var mkdirp = require('mkdirp');
-var es = require('event-stream');
+var ns = require('node-stream');
 var _ = require('lodash');
 
 var MODULES = ['mocha', 'chai', 'istanbul'];
@@ -18,20 +18,21 @@ module.exports = function gitInit(opts, done) {
 
     async.series([
         function checkExistingModules(next) {
-            fs.createReadStream(pkgPath).pipe(es.wait(function (err, body) {
-                var pkg;
-
-                try {
-                    pkg = JSON.parse(body.toString());
-                } catch (e) {
-                    return next(e);
+            ns.wait.json(fs.createReadStream(pkgPath), function (err, body) {
+                if (err && err.code === 'ENOENT') {
+                    // package.json doesn't exist, so we can move on
+                    return next();
                 }
 
-                var devDependencies = _.keys(pkg.devDependencies || {});
+                if (err) {
+                    return next(err);
+                }
+
+                var devDependencies = _.keys(body.devDependencies || {});
                 modules = _.difference(modules, devDependencies);
 
                 next();
-            }));
+            });
         },
         function installStuff(next) {
             if (!modules.length) {
@@ -48,41 +49,28 @@ module.exports = function gitInit(opts, done) {
             });
         },
         function addPackageScripts(next) {
-            fs.createReadStream(pkgPath).pipe(es.wait(function (err, body) {
-                if (err) {
-                    // we will actually ignore the error
-                    return next();
-                }
-            })).pipe(es.map(function (data, cb) {
-                var jsonData;
+            ns.wait.json(fs.createReadStream(pkgPath), function (err, body) {
 
-                try {
-                    jsonData = JSON.parse(data.toString());
-                } catch (e) {
-                    return cb(e);
-                }
-
-                var testScript = _.get(jsonData, 'scripts.test', null);
-
-                if (testScript && /echo .* \&\& exit 1/.test(testScript)) {
-                    delete jsonData.scripts.test;
-                }
-
-                jsonData.scripts = _.merge({
-                    test: 'mocha',
-                    coverage: 'istanbul cover --dir coverage node_modules/mocha/bin/_mocha'
-                }, jsonData.scripts || {});
-
-                cb(null, JSON.stringify(jsonData, null, 2));
-            })).pipe(es.wait(function (err, body) {
-                if (err) {
-                    // this error is real, so we should use it
+                if (err && err.code === 'ENOENT') {
+                    body = {};
+                } else if (err) {
                     return next(err);
                 }
 
+                var testScript = _.get(body, 'scripts.test', null);
+
+                if (testScript && /echo .* \&\& exit 1/.test(testScript)) {
+                    delete body.scripts.test;
+                }
+
+                body.scripts = _.merge({
+                    test: 'mocha',
+                    coverage: 'istanbul cover --dir coverage node_modules/mocha/bin/_mocha'
+                }, body.scripts || {});
+
                 // We can't just stream the data into an fs.createWriteStream :(
-                fs.writeFile(pkgPath, body, next);
-            }));
+                fs.writeFile(pkgPath, JSON.stringify(body, null, 2) + '\n', next);
+            });
         },
         function makeTestFolder(next) {
             mkdirp(path.resolve(process.cwd(), 'test'), next);
