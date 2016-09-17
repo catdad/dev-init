5f84923c05629e0070501c6be93fb81dc494a6bc
Kiril Vatev <vatev.1@gmail.com>
2016-09-17 10:37:14 -0400

    deleting commented out stuff

--- src/npm-init.js
+++ src/npm-init.js
@@ -3,8 +3,6 @@
 var util = require('util');
 var child = require('child_process');
 
-//var shellton = require('shellton');
-
 function spawn(cmd, args, callback) {
     var child = require('child_process').spawn(cmd, args, {
         cwd: process.cwd(),
@@ -25,16 +23,6 @@ function spawn(cmd, args, callback) {
 }
 
 module.exports = function gitInit(opts, done) {
-//    shellton.exec({
-//        task: 'npm init',
-//        cwd: process.cwd(),
-//        stdin: process.stdin,
-//        stdout: process.stdout,
-//        stderr: process.stderr
-//    }, function(err, stdout, stderr) {
-//        done(err);
-//    });
-
     var npm = /^win[0-9]/.test(process.platform) ? 'npm.cmd' : 'npm';
     spawn(npm, ['init', '--yes'], done);
 };
